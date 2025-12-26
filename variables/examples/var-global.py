94a7e53f839ce7196de8b5545ae8683e26f0e47f
ravindrarockz9999 <ravindrarockz9999@gmail.com>
2025-12-26 11:37:20 +0000

    variables

---
+++ variables/examples/var-global.py
@@ -0,0 +1,14 @@
+# Example of Global variable
+a = 10
+b = 5
+
+def add():
+    print(a+b)
+
+def sub():
+    print(a-b)
+
+add()
+sub()
+
+# In Global variables we can assign values onces and use across any functions in the code.
---
+++ variables/examples/var-local.py
@@ -0,0 +1,15 @@
+# Example of local variable
+def add():
+    a = 10 
+    b = 5
+    print(a+b)
+
+def sub():
+    a=15
+    b=20
+    print(a-b)
+
+add()
+sub()
+
+# In local we need to pass the variable inside the function and we can calls within the functions only.
